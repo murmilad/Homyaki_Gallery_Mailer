764940b9e54d25b636be89329d167442b79a8135
Alexey Kosarev <alex@homyaki.info>
2012-03-09 00:04:38 +0400

    Initial  project version

---
+++ db/update_user.sql
@@ -0,0 +1,24 @@
+
+
+UPDATE users 
+	SET  name_call = 'Олега'
+WHERE  email = 'OlegKurgan80@mail.ru'
+
+
+
+/*
+UPDATE users 
+	SET  subject = 'Фоты от хомяков'
+WHERE  email = 'pna79@mail.ru'
+*/
+
+/*
+#	(name, name_call, email, subject, active)
+#	('Нина', 'Нин', 'pna79@mail.ru', 'Накопились тут фоты :) Шлю спам :)',1)
+#	('Лёша Бусыгин', 'Лёша', 'Lexx28@mail.ru', 'Спамлю фотографиями :)',1),
+#	('Рома Коротовских', 'Ром', 'rkorotovskikh@mail.ru', 'Спамлю фотографиями :)',1),
+#	('Олег Шишминцев', 'Олег', 'OlegKurgan80@mail.ru', 'Шлю фотографии :)',1),
+#	('Серега Павлов', 'Серега', 'laesod@ms.tusur.ru', 'Спамлю фотографиями :)',1),
+#	('Серега Потеряев', 'Серу', 'serg_p111@mail.ru', 'У! Серу, фоты шлю :)',1),
+#	('Лёху на Работе', 'Лёху на работе', 'kosareva@rusfinance.ru', 'У! Лёху, фоты шлю :)',1);
+*/

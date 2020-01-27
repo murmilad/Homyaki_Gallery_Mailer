21c2cbe11096a72c3ef362ad3d60b17480563242
alexey <murmilad@gmail.com>
2020-01-27 21:40:36 +0300

    удалено:       db/create_new_user.sql 	удалено:       db/create_sender_tables.sql 	удалено:       db/update_user.sql 	удалено:       resources/tmpl/mail_attachment.tmpl 	удалено:       resources/tmpl/mail_body.tmpl 	удалено:       resources/tmpl/mail_body_news.tmpl 	удалено:       resources/tmpl/mail_container.tmpl 	удалено:       resources/tmpl/mail_picture_info.tmpl 	удалено:       scripts/get_resume.pl 	удалено:       scripts/photo_sender.pl 	удалено:       scripts/send_unsent.sh 	удалено:       scripts/tmp.sql 	удалено:       scripts/update_base_by_unsent_xml.sh

--- scripts/send_unsent.sh
+++
@@ -1,1 +0,0 @@
-for i in `ls /home/alex/Scripts/gf_mail/data/ | grep -P '132278\d{4}.eml'`; do cat /home/alex/Scripts/gf_mail/data/$i | /usr/sbin/sendmail -f alex\@homyaki.info -t; done
--- scripts/update_base_by_unsent_xml.sh
+++
@@ -1,7 +0,0 @@
-
-echo > tmp.sql;
-
-for i in `grep -oP 'acoll_\d{7}[^"]*' $1 | uniq`; do echo  " update images set new_image = 1 where name = '$i';" >> tmp.sql; done;
-
-mysql --defaults-file=/etc/mysqldump.cnf sender < tmp.sql;
-
